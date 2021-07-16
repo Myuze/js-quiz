85420303016d68076f426d6658cbcd09b6199c4f
Florian Meneses <florian.meneses@gmail.com>
2021-07-16 01:11:50 -0700

    initial commit

---
+++ Assets/js/javascript.js
@@ -0,0 +1,27 @@
+// Initialize with a dynamic start page with Start Button
+
+// Click a button to start the quiz
+
+// Timer starts counting down
+
+// Page Shows a random question with a list of answers
+
+// When I select an answer the correct answer is highlighted green
+// and "Correct" appears at the bottom of the list"
+
+// IF I select the wrong answer, the correct answer is highlighted green
+// and the wrong answer turns red
+// and "wrong" appears at the bottom of the list
+// and time is subtracted from the clock
+
+// After each answer I am presented with another question until the end of the quiz
+// or time runs out
+
+// Show Game Over at the end
+// Show final score
+
+// I can save my initials and score
+
+function startTimer() {
+  
+}
